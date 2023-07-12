fb01c1757add716e3b2c124c4257b836bd6b9e45
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 15:51:50 +0800

    Declare variables and show final statements.

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    /* Declare variables */
+    char color[45];
+    char pluralNoun[45];
+    char celebrity[45];
+    /* Show statements */
+    printf("Roses are %s.\n", color);
+    printf("%s are blue.\n", pluralNoun);
+    printf("I love %s.\n", celebrity);
     return 0;
 }
