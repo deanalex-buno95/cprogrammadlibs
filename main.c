7194b379b4c81e9f46efec76d47b11e4f1da1bd2
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 15:59:53 +0800

    Get variable inputs and show final statements.

--- main.c
+++ main.c
@@ -5,10 +5,18 @@ int main() {
     /* Declare variables */
     char color[45];
     char pluralNoun[45];
-    char celebrity[45];
+    char celebrityFirstName[45];
+    char celebrityLastName[45];
+    /* Get variable inputs */
+    printf("Enter a color: ");
+    scanf("%s", color);
+    printf("Enter a plural noun: ");
+    scanf("%s", pluralNoun);
+    printf("Enter a celebrity: ");
+    scanf("%s%s", celebrityFirstName, celebrityLastName);
     /* Show statements */
     printf("Roses are %s.\n", color);
     printf("%s are blue.\n", pluralNoun);
-    printf("I love %s.\n", celebrity);
+    printf("I love %s %s.\n", celebrityFirstName, celebrityLastName);
     return 0;
 }
